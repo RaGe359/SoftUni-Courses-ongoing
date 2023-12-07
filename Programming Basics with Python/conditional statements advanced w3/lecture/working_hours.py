2a086d54c283f5744ecf3a7b78071bbd6ad576fe
Radoslav Georgiev <123582225+RaGe359@users.noreply.github.com>
2023-12-07 22:44:25 +0200

    Delete Programming Basics with Python/conditional statements advanced w3 directory

--- Programming Basics with Python/conditional statements advanced w3/lecture/working_hours.py
+++
@@ -1,8 +0,0 @@
-hour = int(input())
-day = input()
-list1 = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-if hour in range(10,19) and day in list1:
-    print("open")
-else:
-    print("closed")
