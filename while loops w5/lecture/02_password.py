460a2ecd85379e866b0975c3adae0988afc709af
Radoslav Georgiev <123582225+RaGe359@users.noreply.github.com>
2023-12-07 22:11:41 +0200

    Delete while loops w5 directory

--- while loops w5/lecture/02_password.py
+++
@@ -1,9 +0,0 @@
-username = input()
-password = input()
-
-while True:
-    current_password = input()
-
-    if current_password == password:
-        print(f'Welcome {username}!')
-        break
--- while loops w5/lecture/03_sum_numbers.py
+++
@@ -1,8 +0,0 @@
-number = int(input())
-sum_numbers = 0
-
-while sum_numbers < number:
-    num_enter = int(input())
-    sum_numbers += num_enter
-
-print(sum_numbers)
