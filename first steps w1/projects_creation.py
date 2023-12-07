a488d44dddffdb8bf79240f83a63074343d6e325
Radoslav Georgiev <123582225+RaGe359@users.noreply.github.com>
2023-12-07 22:09:59 +0200

    Delete first steps w1 directory

--- first steps w1/projects_creation.py
+++
@@ -1,9 +0,0 @@
-project_time = 3
-
-architect_name = input()
-
-project_amount = int(input())
-
-needed_hours = project_time * project_amount
-
-print(f'The architect {architect_name} will need {needed_hours} hours to complete {project_amount} project/s.')
